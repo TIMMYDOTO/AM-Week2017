bd00d27eb5ef8311f60dd4acd4f7917a33691f1e
Artiom Schiopu <Artiom.Schiopu@endava.com>
2017-04-19 18:14:14 +0300

    commit

---
+++ AM Week/ScheduleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleTableViewController.h
+//  AM Week
+//
+//  Created by Artiom Schiopu on 4/19/17.
+//  Copyright © 2017 Artiom Schiopu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleTableViewController : UIViewController
+
+@end
